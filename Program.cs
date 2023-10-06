c630c1a0a33aea815bf56ae490356305286a4888
RomyKlein <136070990+RomyKlein@users.noreply.github.com>
2023-10-06 11:19:01 +0300

    Files added

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿string[] GetArrayStringConsole (string inConsolSimb)
+{
+    string[] arraySimb = new string[inConsolSimb.Length];
+    arraySimb = inConsolSimb.Split(",");
+    return arraySimb;
+}
+
+string[] GetArraySort (string[] array)
+{
+    int count = 0;
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simb = array[i];
+        if (simb.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayPerfSimb = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simb = array[i];
+        if (simb.Length <= 3)
+        {
+            arrayPerfSimb[j] = simb;
+            j++;
+        }
+    }
+    return arrayPerfSimb;
+}
+
+void Print (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array[i]}, ");
+    }
+}
+
+Console.Write ("Please print your elements with split (,): ");
+string userSimbols = Console.ReadLine();
+string[] arraySimb = GetArrayStringConsole(userSimbols);
+Console.Write ("Your elements were sorted and now you've got new string with elements that have got 3 or less simbols in it: ");
+Print (GetArraySort (arraySimb));
+
+
